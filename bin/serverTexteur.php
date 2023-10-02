5b178b358f35283e7caaba0b8ba05ae84f0043ba
Tiana-Andria <tiana00.manao@gmail.com>
2023-10-02 08:46:35 +0300

    first commit

---
+++ bin/serverTexteur.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace serverTexteur;
+
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+use MyApp\Chat;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+
+class serverTexteur {
+
+    public static function createServer() {
+
+            $server = IoServer::factory(
+                new HttpServer(
+                    new WsServer(
+                        new Chat()
+                    )
+                ),
+                9093
+            );
+
+            $server->run();
+    }
+
+}
+
+serverTexteur::createServer();
